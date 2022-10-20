0e5bee59346315bc1175362a58591406f1b9e58e
Osadchyi Dmytro <osadchyidmitrii@gmail.com>
2022-10-20 13:18:08 +0300

    Отримав файл з переліком білетів від замовника, та запарсив його в строковому вигляді з exception перевіркою

--- src/CSVParser.java
+++ src/CSVParser.java
@@ -4,32 +4,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVParser {
-
-    public String[] DataParser() throws IOException {
+    public String[] DataParser() {
         String line = "";
         String splitBy = ",";
-        String[] ivents = new String[0];
+        String[] ivents = new String[]{};
         BufferedReader br = null;
+        String fileName = "AllIvents.csv";
+        String errorMassage = "Error reading a file: ";
         try {
-            //parsing a CSV file
-            br = new BufferedReader(new FileReader("AllIvents.csv"));
-            while ((line = br.readLine()) != null)   //check are we have a next line
-            {
-                ivents = line.split(splitBy);
+            br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null) {
+                return ivents = line.split(splitBy);
             }
         } catch (IOException e) {
-            e.printStackTrace();// The printStackTrace() method in Java
-            // is a tool used to handle exceptions and errors.
-            // It is a method of Java’s throwable class which prints
-            // the throwable along with other details like the
-            // line number and class name where the exception occurred.
-        } finally {
-            assert br != null;
-            br.close();
+            e.printStackTrace();
+            System.out.println(errorMassage + fileName);
         }
         return ivents;
     }
 }
-/*I add a new class CSVParser and method DataParser*/
+
 
 
